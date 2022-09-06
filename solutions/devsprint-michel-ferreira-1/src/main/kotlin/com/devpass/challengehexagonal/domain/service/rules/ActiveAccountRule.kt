fa06f06ac41079509cc973367252510e569e7e06
elaine mattos <elaine.mattos@outlook.pt>
2022-09-06 16:48:28 -0300

    remove AccountRepositoryPort

--- solutions/devsprint-michel-ferreira-1/src/main/kotlin/com/devpass/challengehexagonal/domain/service/rules/ActiveAccountRule.kt
+++ solutions/devsprint-michel-ferreira-1/src/main/kotlin/com/devpass/challengehexagonal/domain/service/rules/ActiveAccountRule.kt
@@ -3,15 +3,11 @@ package com.devpass.challengehexagonal.domain.service.rules
 import com.devpass.challengehexagonal.domain.model.Account
 import com.devpass.challengehexagonal.domain.model.ValidationResult
 import com.devpass.challengehexagonal.domain.model.Violation
-import com.devpass.challengehexagonal.domain.ports.AccountRepositoryPort
 
-class ActiveAccountRule(
-    private val accountRepositoryPort: AccountRepositoryPort
-) {
+class ActiveAccountRule {
 
     fun validate(account: Account): ValidationResult {
         val validationResult = ValidationResult()
-        val account = accountRepositoryPort.getAccount(accountId = account.id!!)
 
         if (!account.active) {
             validationResult.violations.add(Violation.ACCOUNT_NOT_ACTIVE)
