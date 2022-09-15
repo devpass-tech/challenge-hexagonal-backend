d21ab82b320a526ebc9e9ded61c5dc8fe1904205
livioqueiroz <queiroz.livio@gmail.com>
2022-09-15 17:18:23 -0300

    create CreditCardEntity

--- solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/data/entities/CreditCardEntity.kt
+++ solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/data/entities/CreditCardEntity.kt
@@ -1,11 +1,24 @@
 package io.devpass.creditcard.data.entities
 
+import java.time.LocalDateTime
+import org.hibernate.annotations.CreationTimestamp
+import org.jetbrains.annotations.NotNull
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
 data class CreditCardEntity(
     @Id val id: String,
+    val owner: String,
+    val number: String,
+    val securityCode: String,
+    val printedName: String,
+    val credit_limit: Double,
+    val availableCreditLimit: Double,
+    @CreationTimestamp
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @CreationTimestamp
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
 ) {
 
     // fun toCreditCard() : CreditCard {} // This function will return the domain object
