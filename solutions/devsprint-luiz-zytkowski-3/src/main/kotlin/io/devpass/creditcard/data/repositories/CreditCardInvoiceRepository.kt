92ee0ac3c901d3cee0d7e1cbf9c6e96b12f877a9
Junior <junior.zytkowski@gmail.com>
2022-09-19 19:53:59 -0300

    Fixed sintax of CreditCardInvoiceRepository

--- solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/data/repositories/CreditCardInvoiceRepository.kt
+++ solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/data/repositories/CreditCardInvoiceRepository.kt
@@ -1,9 +1,10 @@
 package io.devpass.creditcard.data.repositories
 
+import io.devpass.creditcard.data.entities.CreditCardInvoiceEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CreditCardInvoiceRepository : CrudRepository<T, ID> {
+interface CreditCardInvoiceRepository : CrudRepository<CreditCardInvoiceEntity, String> {
 
 }
