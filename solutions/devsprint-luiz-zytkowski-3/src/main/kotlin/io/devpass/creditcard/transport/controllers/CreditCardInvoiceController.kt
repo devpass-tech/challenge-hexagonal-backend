f27d72fcb978d8ef3e5259ef394ef471a1ee4597
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2022-09-20 19:26:53 -0300

    corrige request mapping

--- solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/transport/controllers/CreditCardInvoiceController.kt
+++ solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/transport/controllers/CreditCardInvoiceController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("creditCardInvoice")
+@RequestMapping("credit-card-invoices")
 class CreditCardInvoiceController(
     private val creditCardInvoiceServiceAdapter: ICreditCardInvoiceServiceAdapter
 ) {
