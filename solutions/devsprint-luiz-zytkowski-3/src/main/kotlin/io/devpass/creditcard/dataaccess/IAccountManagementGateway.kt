63d7400ff23a5979306e3f11ec5187324934f78f
talu <talita.santos96@hotmail.com>
2022-09-21 14:38:26 -0300

    [KT3-16] Implementa consulta de conta por ID no IAccountManagerGateway
    Co-authored-by: pedroajs <pedroajs93@gmail.com>

--- solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/dataaccess/IAccountManagementGateway.kt
+++ solutions/devsprint-luiz-zytkowski-3/src/main/kotlin/io/devpass/creditcard/dataaccess/IAccountManagementGateway.kt
@@ -1,7 +1,8 @@
 package io.devpass.creditcard.dataaccess
-
 import io.devpass.creditcard.domain.objects.accountmanagement.Account
 
 interface IAccountManagementGateway {
     fun getByCPF(CPF: String): Account
+
+    fun getAccountById(accountId : String) : Account
 }
